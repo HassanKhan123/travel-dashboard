e2f95b49ae9e57afd59132e1e6a3171ea46f3234
HassanKhan123 <hk260280@gmail.com>
2025-08-01 18:05:54 +0200

    added: assets and sidebar

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "components";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
-      Admin Layout
-      <Outlet />
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
     </div>
   );
 };
